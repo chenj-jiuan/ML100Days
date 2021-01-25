49d7b9293944e5460e05c5aa42ffb45367b29575
chenj-jiuan <candy135good@gmail.com>
2021-01-25 16:29:06 +0800

    Create Day_003_HW.py

---
+++ Day_003_HW.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## [作業目標]
+# 持續接觸有關機器學習的相關專案與最新技術
+
+# ## [作業重點]
+# 透過觀察頂尖公司的機器學習文章，來了解各公司是怎麼應用機器學習在實際的專案上
+
+# ## [作業]
+# 今天的作業希望大家能夠看看全球機器學習巨頭們在做的機器學習專案。以 google 為例，下圖是 Google 內部專案使用機器學習的數量，隨著時間進展，現在早已超過 2000 個專案在使用機器學習。
+
+# ![image](https://cdn-images-1.medium.com/max/800/1*U_L8qI8RmYS-MOBrYvXhSA.png)
+
+# 底下幫同學整理幾間知名企業的 blog 或機器學習網站 (自行搜尋也可)，這些網站都會整理最新的機器學習專案或者是技術文章，請挑選一篇文章閱讀並試著回答
+# 1. 專案的目標？ (要解決什麼問題）
+# 2. 使用的技術是？ (只需知道名稱即可，例如：使用 CNN 卷積神經網路做影像分類)
+# 3. 資料來源？ 
+
+# - [Google AI blog](https://ai.googleblog.com/)
+# - [Facebook Research blog](https://research.fb.com/blog/)
+# - [Apple machine learning journal](https://machinelearning.apple.com/)
+# - [機器之心](https://www.jiqizhixin.com/)
+# - [雷鋒網](http://www.leiphone.com/category/ai)
+
+# In[ ]:
+
+
+從Facebook Research blog-Building a ubiquitous shared infrastructure using Twine的資料中顯示出
+該專案的目標？ (要解決什麼問題）
+集群管理系統將工作負載分配給電腦，並透過管理工作負載的生命週期的方式來轉換為大規模的共享架構。
+使用的技術是？ (只需知道名稱即可，例如：使用 CNN 卷積神經網路做影像分類)
+使用Twine協作工作負載生命週期管理
+資料來源？
+Facebook Research blog
+
